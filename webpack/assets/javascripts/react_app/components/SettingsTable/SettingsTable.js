zjhuntin/foreman

commit b1b8ae6155146bd9593eec634e8f88ecfe683ab4

Fixes #38255 - settings page table update to pf5

webpack/assets/javascripts/react_app/components/SettingsTable/SettingsTable.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Table } from '../common/table';
+import { Table, Thead, Tr, Th, Tbody, Td } from '@patternfly/react-table';
 
-import createSettingsTableSchema from './SettingsTableSchema';
+import { translate as __ } from '../../common/I18n';
+import SettingValueCell from './components/SettingValueCell';
+import SettingNameCell from './components/SettingName';
 
-const SettingsTable = ({ settings }) => (
-  <Table
-    key="settings-table"
-    columns={createSettingsTableSchema}
-    rows={settings}
-  />
+const TableHead = () => (
+  <Thead>
+    <Tr ouiaId="setting-table-heading-row">
+      <Th width={30}> {__('Name')} </Th>
+      <Th width={35}> {__('Value')} </Th>
+      <Th width={35}> {__('Description')} </Th>
+    </Tr>
+  </Thead>
 );
 
+const ViewRows = ({ settings }) =>
+  settings.map((data, index) => (
+    <Tr key={index} ouiaId={`setting-table-heading-row-${index}`}>
+      <Td>
+        <SettingNameCell setting={data} />
+      </Td>
+      <Td>
+        <SettingValueCell setting={data} index={index} />
+      </Td>
+      <Td>{data.description}</Td>
+    </Tr>
+  ));
+
+const SettingsTable = ({ settings }) => (
+  <Table isStriped variant="compact" ouiaId="settings-table" borders>
+    <TableHead />
+    <Tbody>
+      <ViewRows settings={settings} />
+    </Tbody>
+  </Table>
+);
 SettingsTable.propTypes = {
   settings: PropTypes.array.isRequired,
 };

app/views/settings/index.html.erb
@@ -31,5 +31,3 @@
     </div>
   <% end %>
 </div>
-
-<%= react_component('SettingUpdateModal') %>

test/integration/setting_js_test.rb
@@ -29,4 +29,77 @@ class SettingJSTest < IntegrationTestWithJavascript
     assert_index_page(settings_path, "Settings", false, true, false)
     assert page.has_link?("My Pretty Setting Label", :href => "#category_label_test_settings_tab")
   end
+
+  test "general headers values" do
+    visit settings_path
+
+    assert_text 'Name'
+    assert_text 'Value'
+    assert_text 'Description'
+  end
+
+  test "edit single arrays values" do
+    visit settings_path
+    click_button 'http_proxy_except_list'
+
+    input = find('textarea#setting-textarea-http_proxy_except_list')
+    assert_empty input.value
+
+    input.fill_in with: 'testArr'
+    find(:ouia_component_id, 'submit-edit-btn').click
+    assert_text 'updated setting'
+
+    assert_text '[testArr]'
+  end
+
+  test "edit empty array values" do
+    visit settings_path
+    click_button 'http_proxy_except_list'
+
+    input = find('textarea#setting-textarea-http_proxy_except_list')
+    assert_empty input.value
+
+    input.fill_in with: ''
+    find(:ouia_component_id, 'submit-edit-btn').click
+    assert_text 'updated setting'
+
+    assert_text '[]'
+  end
+
+  test "edit multi array values" do
+    visit settings_path
+    click_button 'http_proxy_except_list'
+
+    input = find('textarea#setting-textarea-http_proxy_except_list')
+    assert_empty input.value
+
+    input.fill_in with: 'testArr,testArr2'
+    find(:ouia_component_id, 'submit-edit-btn').click
+    assert_text 'updated setting'
+
+    assert_text '[testArr, testArr2]'
+  end
+
+  test "string input type" do
+    visit settings_path
+    click_button 'entries_per_page'
+
+    input = find('input#setting-input-entries_per_page')
+    assert_equal input.value, '20'
+
+    input.fill_in with: '25'
+    find(:ouia_component_id, 'submit-edit-btn').click
+
+    assert_text '25'
+  end
+
+  test "select input type" do
+    visit settings_path
+    click_button 'default_timezone'
+
+    select "(GMT +01:00) Berlin", from: 'setting-select-default_timezone'
+    find(:ouia_component_id, 'submit-edit-btn').click
+
+    assert_text '(GMT +01:00) Berlin'
+  end
 end

test/integration_test_helper.rb
@@ -393,6 +393,9 @@ class IntegrationTestWithJavascript < ActionDispatch::IntegrationTest
 
   def before_setup
     Capybara.current_driver = Capybara.javascript_driver
+    Capybara.add_selector(:ouia_component_id) do
+      css { |value| %[*[data-ouia-component-id="#{value}"]] }
+    end
     super
   end
 end

webpack/assets/javascripts/react_app/components/SettingUpdateModal/SettingUpdateModal.js
@@ -1,28 +1,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-import ForemanModal from '../ForemanModal';
-import { sprintf, translate as __ } from '../../common/I18n';
-
-import SettingForm from './components/SettingForm';
-
-import { SETTING_UPDATE_MODAL } from './SettingUpdateModalConstants';
-
-const SettingUpdateModal = ({ setting, setModalClosed }) => (
-  <ForemanModal
-    id={SETTING_UPDATE_MODAL}
-    title={sprintf(__('Update value for %s setting'), setting.fullName)}
-    enforceFocus
-  >
-    <div>
-      <SettingForm setting={setting} setModalClosed={setModalClosed} />
-    </div>
-  </ForemanModal>
-);
-
-SettingUpdateModal.propTypes = {
-  setting: PropTypes.object.isRequired,
-  setModalClosed: PropTypes.func.isRequired,
-};
-
-export default SettingUpdateModal;

webpack/assets/javascripts/react_app/components/SettingUpdateModal/SettingUpdateModal.test.js
@@ -1,15 +1,0 @@
-import { testComponentSnapshotsWithFixtures } from 'foremanReact/common/testHelpers';
-
-import { arraySetting } from '../SettingRecords/__tests__/SettingRecords.fixtures';
-
-import SettingUpdateModal from './SettingUpdateModal';
-
-const fixtures = {
-  'it should render': {
-    setting: arraySetting,
-    setModalClosed: () => {},
-  },
-};
-
-describe('SettingUpdateModal', () =>
-  testComponentSnapshotsWithFixtures(SettingUpdateModal, fixtures));

webpack/assets/javascripts/react_app/components/SettingUpdateModal/SettingUpdateModalConstants.js
@@ -1,3 +1,0 @@
-export const SETTING_UPDATE_MODAL = 'settingUpdateModal';
-export const SETTING_UPDATE_PATH = '/api/settings/:id';
-export const SETTING_NEW_HOSTS_PAGE = 'new_hosts_page';

webpack/assets/javascripts/react_app/components/SettingUpdateModal/__snapshots__/SettingUpdateModal.test.js.snap
@@ -1,36 +1,0 @@
-// Jest Snapshot v1, https://goo.gl/fbAQLP
-
-exports[`SettingUpdateModal it should render 1`] = `
-<ConnectedForemanModal
-  enforceFocus={true}
-  id="settingUpdateModal"
-  title="Update value for HTTP(S) proxy except hosts setting"
->
-  <div>
-    <WrappedSettingForm
-      setModalClosed={[Function]}
-      setting={
-        Object {
-          "category": "General",
-          "configFile": "settings.yaml",
-          "createdAt": "2019-11-06 09:42:45 +0100",
-          "default": Array [],
-          "description": "Set hostnames to which requests are not to be proxied. Requests to the local host are excluded by default.",
-          "encrypted": false,
-          "fullName": "HTTP(S) proxy except hosts",
-          "id": 47,
-          "name": "http_proxy_except_list",
-          "readonly": false,
-          "selectValues": null,
-          "settingsType": "array",
-          "updatedAt": "2020-03-20 13:44:40 +0100",
-          "value": Array [
-            "localhost",
-            "127.0.0.1",
-          ],
-        }
-      }
-    />
-  </div>
-</ConnectedForemanModal>
-`;

commit b90ee4b404760e09b48719d7b4ffdd726b59f4f8

SAT-37300 - Setting descriptions aren't translated anymore
Fixes #38735 - Translate setting descriptions in UI

Prior to the rewrite to React and PF5 the descriptions were translated.
This restores that behavior.

Fixes: b1b8ae615514 ("Fixes #38255 - settings page table update to pf5")
(cherry picked from commit 9579d0d6bc5c80737c99e5ea33a2390cf7c6a1fd)

webpack/assets/javascripts/react_app/components/SettingsTable/SettingsTable.js
@@ -28,7 +28,7 @@ const ViewRows = ({ settings }) =>
       <Td width={35}>
         <SettingValueCell setting={data} index={index} />
       </Td>
-      <Td width={35}>{data.description}</Td>
+      <Td width={35}>{__(data.description)}</Td>
     </Tr>
   ));
 